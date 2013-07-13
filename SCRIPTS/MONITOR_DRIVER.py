8636b30ec1a8c8279a8dd8e51f1b3b2808d1bdb0
Nathaniel Chaney <chaneyna@gmail.com>
2013-07-12 20:17:15 -0400

    Added the scripts and libraries

---
+++ SCRIPTS/MONITOR_DRIVER.py
@@ -0,0 +1,23 @@
+#################################################################
+# Drought Monitor V2
+# Written by: Nathaniel W. Chaney
+# Date: 12 July 2013
+# Location: Princeton University
+# Contact: nchaney@princeton.edu
+# Purpose: This scripts drives the realtime drought monitor
+##################################################################
+
+import datetime
+
+def Download_Data():
+
+ #1. Download global GFS analysis fields
+
+
+#1. Determine the period that needs to be updated
+
+#2. Download all the relevant data
+
+#3. Process all the relevant data
+
+#4. Run all the relevant models
