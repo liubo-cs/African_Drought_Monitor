3279a882ffa27e47bd51fce6f0b92955b2574868
Nathaniel Chaney <chaneyna@gmail.com>
2013-08-01 16:06:34 -0400

    Added 30 day moving average to NDVI

--- SCRIPTS/MONITOR_DRIVER.py
+++ SCRIPTS/MONITOR_DRIVER.py
@@ -13,32 +13,6 @@ import numpy as np
 import grads
 import os
 
-'''
-def Download_and_Process_Seasonal_Forecast(date):
-
- models = ('CMC1-CanCM3','CMC2-CanCM4','COLA-RSMAS-CCSM3','GFDL-CM2p1-aer04','NASA-GMAO-062012')
- nensembles = (10,10,6,10,12)
- type = ('.FORECAST/','.FORECAST/','','','')
- vars = ('prec','tref')
- month = date.strftime('%b') 
- year = date.year
- dir0 = '../DATA/SEASONAL_FORECAST/%04d%02d' % (date.year,date.month)
- if not os.path.exists(dir0):
-  os.makedirs(dir0)
- for var in vars:
-  dir = dir0 + '/' + var
-  if not os.path.exists(dir):
-   os.makedirs(dir)
-  i = 0 
-  for model in models:
-   for ens in xrange(1,nensembles[i]+1):
-    root = ''
-    http_file = 'http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/.{0}/{3}.MONTHLY/.{5}/S/%280000%201%20{1}%20{2}%29%280000%201%20{1}%20{2}%29RANGEEDGES/M/%28{4}.%29VALUES/M/removeGRID/S/removeGRID/-9.9900000E08/setmissing_value/data.cdf'.format(model,month,year,type[i],ens,var)
-    file_out = '{0}/{1}_{2}.nc'.format(dir,model,ens)
-    os.system('wget -O {0} {1}'.format(file_out,http_file))
-   i = i + 1
-'''
- 
 #1. Determine the period that needs to be updated
 
 #2. Download all the relevant data
@@ -80,8 +54,8 @@ os.system('../LIBRARIES/gds-2.0/rebootserver')
 '''
 #grads_exe = '../LIBRARIES/grads-2.0.1.oga.1/Contents/grads'
 #ga = grads.GaNum(Bin=grads_exe,Window=False,Echo=False)
-idate = datetime.datetime(2001,1,1)
-fdate = datetime.datetime(2008,12,31)
+idate = datetime.datetime(2012,1,1)
+fdate = datetime.datetime(2012,12,31)
 date = idate
 
 '''
@@ -90,17 +64,26 @@ ctl_in = "../DATA/3B42RT/MONTHLY/3B42RT_monthly_0.25deg.ctl"
 file_out = "../WORKSPACE/parameters.nc"
 dt = datetime.timedelta(days = 31)
 ml.Fit_Distribution('gamma',ctl_in,idate,fdate,dt,'prec',file_out,dims)
-'''
+
 date = datetime.datetime(2013,1,1)
 #Download and process the seasonal forecast
 ml.Download_and_Process_Seasonal_Forecast(date)
 
-
-'''
 print date
 #Download and process modis NDVI
 ml.Download_and_Process_NDVI(date,dims)
 date = date + dt
+
+'''
+date= idate
+dt_ma = datetime.timedelta(days=30)
+dt = datetime.timedelta(days=1)
+while date <= fdate:
+ print date
+ ml.Compute_NDVI_moving_average(date,dt_ma,dims)
+ date = date + dt
+
+'''
 
 while date <= fdate:
  #End of month routines
